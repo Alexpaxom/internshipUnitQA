caec9fa2c223262cb7e8f11d08e9fe165bfdbf34
Alexpaxom <WorkAlexAn@gmail.com>
2019-06-04 16:23:35 +0300

    Парсер для второго лог файла, в качестве основного ключа выбрано время проведения теста в формате unix.
    Основной метод parse принимает путь к файлу,
    -выкидывает стандартные исключения если файл не найден или json не корректен.
    -Также проверяет наличие полей time, name, errors если их нет выкидывает исклчение KeyError
    
    На выходе выдает сформированный словарь, ключеми которого является поля time в unix формате и информацией о каждом тесте

---
+++ SecondFileParser.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from datetime import timezone
+import json
+
+class SFParser:
+    """Parser for second log file"""
+
+    def parse(self, file_path, **params):
+        result = {}
+        with open(file_path) as file:
+            file_data = json.load(file)
+            result = self.parse_json(file_data)
+
+        return result
+
+
+    def parse_json(self, file_data, **params):
+        result = {}
+
+        if 'suites' not in file_data:
+            return result
+
+        for suite in file_data['suites']:
+            suite_cases = self.__parse_suites_log(suite)
+            # if there are no duplicate keys
+            if len(suite_cases.keys() & result.keys()) == 0:
+                result.update(suite_cases)
+            else:
+                raise KeyError('Two tests have the same "time". We cannot distinguish them.')
+
+        return result
+
+    def __parse_suites_log(self, suite, **params):
+        result = {}
+
+        if 'cases' not in suite:
+            return result
+
+        for test_log in suite['cases']:
+            case_test = self.__parse_test_log(test_log)
+            # if there are no duplicate keys
+            if len(case_test.keys() & result.keys()) == 0:
+                result.update(case_test)
+            else:
+                raise KeyError('Two tests have the same "time". We cannot distinguish them.')
+
+        return result
+
+    def __parse_test_log(self, test_log, **params):
+        if 'time' not in test_log:
+            raise KeyError('Field \'time\' is required!')
+
+        result = {}
+        time = datetime.strptime(test_log['time'], '%A, %d-%b-%y %H:%M:%S %Z')
+        time = int(time.replace(tzinfo=timezone.utc).timestamp())
+        result[time] = {}
+
+        if 'name' in test_log:
+            result[time]['name'] = test_log['name']
+        else:
+            raise KeyError('Field \'name\' is required!')
+
+        if 'errors' in test_log:
+            result[time]['status'] = 'errors: ' + str(test_log['errors'])
+        else:
+            raise KeyError('Field \'errors\' is required!')
+
+        return result
---
+++ tests/TestSecondFileParser.py
@@ -0,0 +1,56 @@
+import unittest
+import json
+from SecondFileParser import SFParser
+
+
+class TestSecondFileParser(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        self.parser = SFParser()
+
+
+    def test_correct_files(self):
+        # TEST
+        json_res = json.dumps(self.parser.parse('tests\materials\File_empty_json.json'), sort_keys=True)
+        json_expect = json.dumps({}, sort_keys=True)
+        self.assertEqual(json_res, json_expect)
+
+        # TEST
+        json_res = json.dumps(self.parser.parse('tests\materials\File_2_without_cases.json'), sort_keys=True)
+        json_expect = json.dumps({}, sort_keys=True)
+        self.assertEqual(json_res, json_expect)
+
+        # TEST
+        json_res = json.dumps(self.parser.parse('tests\materials\File_2.json'), sort_keys=True)
+        json_expect = json.dumps({'946684820': {'name': 'Test output B', 'status': 'errors: 0'}}, sort_keys=True)
+        self.assertEqual(json_res, json_expect)
+
+        # TEST
+        json_res = json.dumps(self.parser.parse('tests\materials\File_2_few_records.json'), sort_keys=True)
+        res_expect = {
+            '946684820': {'name': 'Test output B', 'status': 'errors: 0'},
+            '946684821': {'name': 'Test output E', 'status': 'errors: 0'},
+            '946771220': {'name': 'Test output D', 'status': 'errors: 1'}
+        }
+        json_expect = json.dumps(res_expect, sort_keys=True)
+        self.assertEqual(json_res, json_expect)
+
+
+    def test_exceptions(self):
+        # TEST
+        with self.assertRaises(FileNotFoundError):
+            self.parser.parse('not_existing _file')
+
+        # TEST
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            self.parser.parse('tests\materials\File_empty.json')
+
+        # TEST
+        with self.assertRaises(KeyError):
+            self.parser.parse('tests\materials\File_2_without_status_and_name.json')
+
+    def test_fail(self):
+        with self.assertRaises(KeyError):
+            self.parser.parse('tests\materials\File_2_fail_same_time.json')
+
